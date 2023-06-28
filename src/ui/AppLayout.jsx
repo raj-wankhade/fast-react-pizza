f36634847931c568b85f2ef6ef75f3c160121907
raj-wankhade <rajwankhade789@gmail.com>
2023-06-28 10:25:52 +0530

    implementing loader from react router dom's useNavigation

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,21 @@
-import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
+
       <main>
         <Outlet />
       </main>
+
       <CartOverview />
     </div>
   );
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+    </div>
+  );
 }
 
 export default Header;
