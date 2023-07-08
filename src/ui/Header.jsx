e5c5ba1ebb7007f2e9dfeaf646e1b29923e46fe4
raj-wankhade <rajwankhade789@gmail.com>
2023-07-08 19:46:29 +0530

    styling text and colors

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div>
-      <Link to="/" className="tracking-widest">
+    <div className="bg-yellow-500">
+      <Link to="/" className="tracking-widest uppercase">
         Fast React Pizza Co.
       </Link>
       <SearchOrder />
-      <p>raj</p>
+      <Username />
     </div>
   );
 }
